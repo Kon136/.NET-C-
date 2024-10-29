cb9ef46d4f99f1ef8590662df1e9a1f71cd64f7b
Konrad Marczyk <marczykkonrad7@gmail.com>
2024-10-29 16:37:12 +0100

    Add files via upload

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace kalkulator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            float b = float.Parse(Console.ReadLine());
+            float c = 0;
+
+            Console.WriteLine("wybierz typ działania");
+            Console.WriteLine("1-dodawanie");
+            Console.WriteLine("2-odejmowanie");
+            Console.WriteLine("3-mnożenie");
+            Console.WriteLine("4-dzielenie");
+
+            int typDzialania = int.Parse(Console.ReadLine());
+
+            switch (typDzialania)
+            {
+                case 1:
+                    
+                    c = a + b;
+                        Console.WriteLine("Wynik:" + c);
+                        break;
+                case 2:
+                    c = a - b;
+                    Console.WriteLine("Wynik:" + c);
+                    break;
+                case 3:
+                     c = a * b;
+                    Console.WriteLine("Wynik:" + c);
+                    break;
+                case 4:
+                     c = a / b;
+                    Console.WriteLine("Wynik:" + c);
+                    break;
+                default:
+                    Console.WriteLine("Podano złe dane");
+                    break;
+
+
+            };
+            Console.WriteLine("Wciśnij enter aby zakończyć");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
